b46af8d5b97bb66f85d9dbe003cf23eb0d6d61e8
Javad Huseynli <cavadhuseynli98@gmail.com>
2025-10-20 23:45:38 +0400

    restore dark theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
